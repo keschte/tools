fe09680404df5d143a67a6b59daa572203ac8614
Stefan Kestenholz <stefan.kestenholz@sbb.ch>
2019-03-15 13:39:06 +0100

    initial commit

---
+++ BgInfo/bginfo-col-test.cmd
@@ -0,0 +1,1 @@
+C:\COL-Tools\Sysinternals\BgInfo\Bginfo.exe "C:\COL-Tools\Sysinternals\BgInfo\bginfo-col-test.bgi" /timer:0 /silent /nolicprompt
---
+++ BgInfo/set-dev-wallpaper.cmd
@@ -0,0 +1,1 @@
+copy "C:\COL-Tools\Sysinternals\BgInfo\set-gadev-wallpaper.lnk" "C:\Users\Garaio-Admin\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
