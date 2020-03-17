347d84d68de5f3e37071441ad606576524d1130b
Stefan Kestenholz <stefan.kestenholz@sbb.ch>
2020-03-17 09:14:03 +0100

    Garbage collect while updating projects

--- cmder/config/profile.d/SbbGit.ps1
+++ cmder/config/profile.d/SbbGit.ps1
@@ -3,14 +3,19 @@
     Get-ChildItem $rootPath | ForEach-Object {
         try {
             $path = $_.FullName
-            Write-Host "Updating $path ..."
+            Write-Host "------------------ $path -------------------"
             Push-Location $path
-            git pull --ff-only
+
+            git pull --ff-only --prune
+            git gc --prune
+
             Pop-Location
         } catch {
             Write-Error "Failed to update: $_"
         } finally {
             Pop-Location
+            Write-Host
+            Write-Host
         }
     }
 }
